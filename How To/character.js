db0e6ff1d0d9a17deacc5aac26d897f977e2d9ca
Scott Cooper <cooperph@oregonstate.edu>
2017-03-06 20:46:27 -0600

    charcter.html
    updates to character.html and added pictures

--- How To/character.js
+++ How To/character.js
@@ -5,12 +5,7 @@ document.addEventListener('DOMContentLoaded', characterDetails);
 function characterDetails(){
 document.getElementById('urlSubmit').addEventListener('click', function(event){
   var req = new XMLHttpRequest();
-
-  var payload = {realm:null, name:null};
-  payload.realm = document.getElementById('realm').value;
-  payload.name = document.getElementById('charname').value;
-   
-
+  
 //https://us.api.battle.net/wow/character/Sargeras/Maiella?fields=professions&locale=en_US&apikey=a4mttk38ykqdj8ynjw6bzd3hab7sk46s    
   var opening = 'https://us.api.battle.net/wow/character/';
   var serv = document.getElementById('realm').value + '/' + document.getElementById('charname').value;
@@ -20,19 +15,12 @@ document.getElementById('urlSubmit').addEventListener('click', function(event){
   
   req.open('GET', owmURL, false);
   req.setRequestHeader('Content-Type', 'application/json');
-
-  var strObj = JSON.stringify(payload);
   req.send(null);
 
   var response = JSON.parse(req.response);
 
   document.getElementById('cname').textContent = response.name;
-  document.getElementById('myimage').src = ' https://render-api-us.worldofwarcraft.com/static-render/us/' + response.thumbnail;
-  //document.getElementById('repName').textContent = response.reputation[20].name;
-  //document.getElementById('repStanding').textContent = repName(response.reputation[20].standing);
-  //document.getElementById('repValue').textContent = (response.reputation[20].value / response.reputation[20].max * 100).toFixed(2) +'%';
-  
-  // https://render-api-us.worldofwarcraft.com/static-render/us/sargeras/87/137909847-avatar.jpg
+  document.getElementById('myimage').src = 'https://render-api-us.worldofwarcraft.com/static-render/us/' + response.thumbnail;
 
   event.preventDefault();
 })
